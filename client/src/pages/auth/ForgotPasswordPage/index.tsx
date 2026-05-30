69d65469763d722ae2db7cb76555f8a281381cf2
selly361 <seikou615@gmail.com>
2026-05-30 11:00:00 +0100

    feat: add authentication page placeholders

---
+++ client/src/pages/auth/ForgotPasswordPage/index.tsx
@@ -0,0 +1,10 @@
+import { PagePlaceholder } from '../../../components'
+
+export function ForgotPasswordPage() {
+  return (
+    <PagePlaceholder
+      title="Forgotten your password?"
+      description="Enter your email to receive a reset link."
+    />
+  )
+}
---
+++ client/src/pages/auth/ResetPasswordPage/index.tsx
@@ -0,0 +1,10 @@
+import { PagePlaceholder } from '../../../components'
+
+export function ResetPasswordPage() {
+  return (
+    <PagePlaceholder
+      title="Reset Your Password"
+      description="Choose a new password for your account."
+    />
+  )
+}
