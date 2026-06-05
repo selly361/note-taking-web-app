77c50569d537cf96a1b1da4bf778c34484e19a08
selly361 <seikou615@gmail.com>
2026-06-05 10:00:00 +0100

    feat: add theme constants

---
+++ client/src/constants/theme.ts
@@ -0,0 +1,9 @@
+import type { ColorTheme, FontTheme } from '../types'
+
+export const COLOR_THEMES: ColorTheme[] = ['light', 'dark', 'system']
+
+export const FONT_THEMES: FontTheme[] = ['sans', 'serif', 'mono']
+
+export const DEFAULT_COLOR_THEME: ColorTheme = 'system'
+
+export const DEFAULT_FONT_THEME: FontTheme = 'sans'
